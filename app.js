87679579f768bb36427fcf29fdd5b793c04b5ea5
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2023-04-05 13:11:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -181,6 +181,6 @@ app.post("/submit", function (req, res) {
 });
 
 
-app.listen(process.env.PORT|| 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000!");
 })
